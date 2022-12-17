beb852f0a98198b5b56882fe51f9d0dadd8bfe59
tobiasras <tobiasras.job@gmail.com>
2022-12-17 23:31:27 +0100

    created menu box

--- js/router.js
+++ js/router.js
@@ -1,21 +1,84 @@
+$(document).ready(function () {
+    handleLocation("home")
+});
 
-function route(content){
+function route(content) {
     handleLocation(content);
 }
 
 const routes = {
-    404   :         "html/404.html",
-    "home" :        "html/content/main-page.html",
-    "page1" :       "html/content/page1.html",
-    "page2" :       "html/content/page2.html",
+    404: "html/404.html",
+    "home": "html/content/main-page.html",
+    "page1": "html/content/page1.html",
+    "page2": "html/content/page2.html",
 };
+
+
+const mains = {
+    //
+    "home": function main() {
+
+        const pages = {
+            menu1: ["page-1", "page-1-btn"],
+            menu2: ["page-2", "page-2-btn"],
+            menu3: ["page-3", "page-3-btn"]
+        }
+
+        const menu = new Menu(pages, 'box-link', 'box-page', 'active-box-link')
+        menu.showPage('menu1')
+
+
+
+    },
+
+    "page1": function main() {
+
+        $(document).ready(function () {
+
+            console.log(2)
+        });
+
+    },
+
+
+    "page2": function main() {
+
+        $(document).ready(function () {
+            console.log(2)
+        });
+
+    },
+
+
+    404: function main() {
+
+
+        $(document).ready(function () {
+            console.log(3)
+        });
+
+
+    },
+}
+
 
 const handleLocation = async (path) => {
     const route = routes[path] || [404];
-
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("main-content").innerHTML = html;
+
+    mains[path]();
+
 };
 
 // loads the first page
-window.onpopstate = handleLocation("home");
+//window.onpopstate = handleLocation("home");
+
+// prevents defaults when clicking links
+document.addEventListener("click", function (event) {
+    event.preventDefault()
+});
+
+
+
+
---
+++ js/utility/menu.js
@@ -0,0 +1,55 @@
+class Menu {
+
+    /*
+    const pages = {
+        menu1: ["page-1", "page-1-btn"],
+        menu2: ["page-2", "page-2-btn"],
+        menu3: ["page-3", "page-3-btn"]
+    }
+    */
+
+
+
+    constructor(pages, btnClass, boxClass, activeBtnClass) {
+        // used to hide boxes
+        this.boxClass = boxClass;
+        // used to deactivate btn
+
+        this.btnClass = btnClass;
+        this.pages = pages;
+        this.activeBtnClass = activeBtnClass;
+
+        for (let key in pages) {
+            this.listen(key)
+        }
+
+    }
+
+    hideElements(){
+        $('.' + this.btnClass).removeClass(this.activeBtnClass);
+        $('.' + this.boxClass).hide();
+    }
+
+     showPage(page){
+
+         console.log(page);
+
+
+         this.hideElements();
+        $('#' + this.pages[page][0]).show();
+        $('#' + this.pages[page][1]).addClass(this.activeBtnClass);
+    }
+
+
+    listen(page){
+        let menu = this;
+
+
+        $('#' + this.pages[page][1]).click(function(){
+            menu.showPage(page);
+        });
+    }
+
+
+
+}
